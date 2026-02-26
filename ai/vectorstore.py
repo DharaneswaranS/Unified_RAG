a7b5a9a3b6eb2d7d23184a5c58bbb68dd3a787d2
DharaneswaranS <dharaneshwaransaravanan@gmail.com>
2026-02-26 15:01:27 +0530

    Remove backend dependency from vectorstore for Streamlit deploy

--- ai/vectorstore.py
+++ ai/vectorstore.py
@@ -1,17 +1,25 @@
 from langchain_chroma import Chroma
 from ai.embeddings import embeddings
-from backend.config import VECTOR_DB_PATH
 import os
+
+# Local persistent directory (inside Streamlit container)
+VECTOR_DB_PATH = "chroma_db"
 os.makedirs(VECTOR_DB_PATH, exist_ok=True)
+
+
 def get_vectorstore(collection_name: str):
     return Chroma(
         persist_directory=VECTOR_DB_PATH,
         collection_name=collection_name,
         embedding_function=embeddings
     )
+
+
 def add_documents(docs, collection_name: str):
     vectordb = get_vectorstore(collection_name)
     vectordb.add_documents(docs)
+
+
 def similarity_search(query, collection_name: str, k=5):
     vectordb = get_vectorstore(collection_name)
     return vectordb.similarity_search(query, k=k)
